2fdde57a16c8641868c28994131abddd4cb56d8f
Karekin <karekin.kiyici@dawsoncollege.qc.ca>
2023-12-14 18:35:55 -0500

    Knockback is mostly complete!

--- Hitbox.gd
+++ Hitbox.gd
@@ -2,6 +2,8 @@ class_name Hitbox
 extends Area2D
 #Exporting a var allows you to change it in the Inspector window
 @export var damage = 10
+@export var knockback = 0
+@export var angle = 0
 #Check Hurtbox.gd for details
 func _init() -> void:
 	collision_layer = 2
--- character_knockback.gd
+++ character_knockback.gd
@@ -5,7 +5,7 @@ extends Node
 
 #Angle must be >= 0
 func get_knockback(base_knockback, angle):
-	var velocity_y = sin(angle) * base_knockback
-	var velocity_x = cos(angle) * base_knockback
+	var velocity_y = sin(deg_to_rad(angle)) * base_knockback
+	var velocity_x = cos(deg_to_rad(angle)) * base_knockback
 	
 	return Vector2(velocity_x, velocity_y)
